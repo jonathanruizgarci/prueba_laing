152ea859ea978654d45e789dcb3adc38970dff5f
jesus.velazquez <jesus.velazquez@sunpower.mx>
2025-11-27 16:45:56 +0000

    Actualizacion .env falla

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
-
-// 1. Vite busca automáticamente en el archivo .env las variables que empiezan con VITE_
+// 1. Vite busca automáticamente en el archivo .env las variables que empiezan con VITE
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+//prueba error de conexión a supabase
+console.log("URL Supabase:", supabaseUrl);
+console.log("Key Supabase:", supabaseKey);
 
 // 2. Usamos esas variables para conectar
-export const supabase = createClient(supabaseUrl, supabaseKey);s
+export const supabase = createClient(supabaseUrl, supabaseKey);
